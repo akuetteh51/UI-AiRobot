ba1e9b2ab228aecbc93707cda8a14a7883c635da
akuetteh51 <akuetteh58@gmail.com>
2021-07-09 22:58:32 -0400

    new file: Caruibot/Car.py new file: Caruibot/coco.names new 	file: Caruibot/detect.py new file: Caruibot/file.html new file: 	Caruibot/frozen_inference_graph.pb new file: 	Caruibot/history.txt new file: Caruibot/mg.jpg new file: 	Caruibot/nk.jpg new file: Caruibot/obj-detect.py new file: 	Caruibot/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt new file: 	Caruibot/static/button.png new file: Caruibot/static/canvas.png 	new file: Caruibot/static/jquery-3.6.0.min.js new file: 	Caruibot/static/script.js new file: Caruibot/templates/app.html

---
+++ Caruibot/obj-detect.py
@@ -0,0 +1,51 @@
+import cv2
+from flask import Flask, render_template, Response
+app = Flask(__name__)
+vid=cv2.VideoCapture(1)
+vid.set(3,640)
+vid.set(4,480)
+#img = cv2.resize(img, (0,0), fx=0.9, fy=0.5)
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('app.html')
+
+
+
+def gen():
+    classNames=[]
+    classfile='coco.names'
+    with open(classfile,'rt') as f:
+        classNames=f.read().rstrip('\n').split('\n')
+    #print(classNames)    
+    configPath='ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+    weightsPath='frozen_inference_graph.pb'
+    net=cv2.dnn_DetectionModel(weightsPath,configPath)
+    net.setInputSize(320,320)
+    net.setInputScale(1.0/127.5)
+    net.setInputMean((127.5,127.5,127.7))
+    net.setInputSwapRB(True)
+    while True:
+        success,img=vid.read()
+        classIds,confs,bbox=net.detect(img,confThreshold=0.5)
+        print(classIds,bbox)
+        
+        if len(classIds) !=0:
+            for classId,confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+                cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+                            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+    #cv2.imshow("Output",img)
+    #cv2.waitKey(0)
+        
+        frame = cv2.imencode('.jpg', img)[1].tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
